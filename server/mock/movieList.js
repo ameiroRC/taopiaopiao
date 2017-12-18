78218690f320128daab985426534ac8fc68690ba
didiaoyao <18911875231@163.com>
2017-12-18 23:59:10 +0800

    one

---
+++ server/mock/movieList.js
@@ -0,0 +1,157 @@
+module.exports = {
+    "hasMore" : true,
+    "list" : [
+        {
+            "id" : 0,
+            "img" : "https://gw.alicdn.com/i3/TB12V0Iiv6H8KJjSspmXXb2WXXa_.jpg_160x160Q75.jpg",
+            "video" : "http://vodcdn.video.taobao.com/oss/taobao-ugc/190d24aad9cc494ea17f3d4bda8c8502/1505302783/video.mp4",
+            "title" : "芳华",
+            "is3D" : false,
+            "grade" : "8.9",
+            "director" : "导演：冯小刚",
+            "starring" : "主演：黄轩 苗苗 钟楚曦 杨采钰"
+        },
+        {
+            "id" : 1,
+            "img" : "https://gw.alicdn.com/i2/TB1xsowaG_85uJjSZFlXXXemXXa_.jpg_160x160Q75.jpg",
+            "video" : "http://vodcdn.video.taobao.com/oss/taobao-ugc/a7b0884ff669408bab77434715b35ad5/1510195018/video.mp4",
+            "title" : "寻梦环游记",
+            "is3D" : true,
+            "grade" : "9.5",
+            "director" : "导演：李·昂克里奇",
+            "starring" : "主演：安东尼·冈萨雷兹"
+        },
+        {
+            "id" : 2,
+            "img" : "https://gw.alicdn.com/i3/TB1g5qXc0HO8KJjSZFHXXbWJFXa_.jpg_160x160Q75.jpg",
+            "video" : "http://vodcdn.video.taobao.com/oss/taobao-ugc/a8d30a99222a47589bc83f16ea2f52fa/1513134375/video.mp4",
+            "title" : "奇门遁甲",
+            "is3D" : true,
+            "grade" : "7.7",
+            "director" : "导演：袁和平",
+            "starring" : "主演：大鹏 倪妮 李治廷 周冬雨"
+        },
+        {
+            "id" : 3,
+            "img" : "https://gw.alicdn.com/i1/TB1GNNGiDnI8KJjSszgXXc8ApXa_.jpg_160x160Q75.jpg",
+            "video" : "http://vodcdn.video.taobao.com/oss/taobao-ugc/612df43174984b5cbd59e414ee35ee7e/1513248916/video.mp4",
+            "title" : "机器之血",
+            "is3D" : true,
+            "grade" : "8.0",
+            "director" : "导演：张立嘉",
+            "starring" : "主演：成龙 罗志祥 欧阳娜娜"
+        },
+        {
+            "id": 4,
+            "img": "https://gw.alicdn.com/i1/TB1PkLjgtzJ8KJjSspkXXbF7VXa_.jpg_160x160Q75.jpg",
+            "video": "http://vodcdn.video.taobao.com/oss/taobao-ugc/a8d8c2e447d449329b41f0cbb17996f0/1512614916/video.mp4",
+            "title": "帕丁顿熊2",
+            "is3D": false,
+            "grade": "9.1",
+            "director": "导演：保罗·金",
+            "starring": "主演：本·威士肖 杜江"
+        },
+        {
+            "id": 5,
+            "img": "https://gw.alicdn.com/i4/TB1lgE8ennI8KJjSszbXXb4KFXa_.jpg_160x160Q75.jpg",
+            "video": "http://vodcdn.video.taobao.com/oss/taobao-ugc/88a374a3a585401ca48790d7d7371b79/1512704553/video.mp4",
+            "title": "至爱梵高·星空之谜2",
+            "is3D": false,
+            "grade": "9.0",
+            "director": "导演：多洛塔·科别拉",
+            "starring": "主演：道格拉斯·布斯 赵天宇"
+        },
+        {
+            "id": 6,
+            "img": "https://gw.alicdn.com/i1/TB1yrlffJzJ8KJjSspkXXbF7VXa_.jpg_160x160Q75.jpg",
+            "video": "http://vodcdn.video.taobao.com/oss/taobao-ugc/d289798376ce43dc86813f834e86c73f/1513166731/video.mp4",
+            "title": "心理罪之城市之光",
+            "is3D": false,
+            "grade": "9.0",
+            "director": "导演：徐纪周",
+            "starring": "主演：邓超 阮经天 刘诗诗"
+        },
+        {
+            "id": 7,
+            "img": "https://gw.alicdn.com/i2/TB1u78.iv6H8KJjy0FjXXaXepXa_.jpg_160x160Q75.jpg",
+            "video": "http://vodcdn.video.taobao.com/oss/taobao-ugc/004ae72b37534b0dab02a3c64bb7ac0c/1513301812/video.mp4",
+            "title": "妖猫传",
+            "is3D": false,
+            "grade": "8.4",
+            "director": "导演：陈凯歌",
+            "starring": "主演：黄轩 染谷将太 张雨绮"
+        },
+        {
+            "id": 8,
+            "img": "https://gw.alicdn.com/i1/TB1.dS0bCYH8KJjSspdXXcRgVXa_.jpg_160x160Q75.jpg",
+            "video": "http://vodcdn.video.taobao.com/oss/taobao-ugc/ac9252d2d5484b398e9f86ea0e1e7db8/1511752285/video.mp4",
+            "title": "至暗时刻",
+            "is3D": false,
+            "grade": "9.0",
+            "director": "导演：乔·赖特",
+            "starring": "主演：加里·奥德曼 莉莉·詹姆斯"
+        },
+        {
+            "id": 9,
+            "img": "https://gw.alicdn.com/i3/TB1g6bqeRTH8KJjy0FiXXcRsXXa_.jpg_160x160Q75.jpg",
+            "video": "http://vodcdn.video.taobao.com/oss/taobao-ugc/48a8e411a30649759e8884bac3e57001/1512960569/video.mp4",
+            "title": "圣诞奇妙公司",
+            "is3D": false,
+            "grade": "8.3",
+            "director": "导演：阿兰·夏巴",
+            "starring": "主演：奥黛丽·塔图"
+        },
+        {
+            "id": 10,
+            "img": "https://gw.alicdn.com/i2/TB1R9WjhdrJ8KJjSspaXXXuKpXa_.jpg_160x160Q75.jpg",
+            "video": "http://vodcdn.video.taobao.com/oss/taobao-ugc/ff583eef6605444e9e09ea305498d5a6/1511490801/video.mp4",
+            "title": "二代妖精之今生有幸",
+            "is3D": false,
+            "grade": "8.6",
+            "director": "导演：肖洋",
+            "starring": "主演：冯绍峰 刘亦菲 李光洁"
+        },
+        {
+            "id": 11,
+            "img": "https://gw.alicdn.com/i4/TB1_xJQf8DH8KJjy1zeXXXjepXa_.jpg_160x160Q75.jpg",
+            "video": "http://vodcdn.video.taobao.com/oss/taobao-ugc/21a7d1e65e964a03983cbd7571bd867c/1511962096/video.mp4",
+            "title": "妖铃铃",
+            "is3D": false,
+            "grade": "8.9",
+            "director": "导演：吴君如",
+            "starring": "主演：吴君如 沈腾 岳云鹏 张译"
+        },
+        {
+            "id": 12,
+            "img": "https://gw.alicdn.com/i3/TB11mucdDnI8KJjSszbXXb4KFXa_.jpg_160x160Q75.jpg",
+            "video": "http://vodcdn.video.taobao.com/oss/taobao-ugc/32e12b534f9f4255ac3b9ae06050645b/1512005633/video.mp4",
+            "title": "巨额来电",
+            "is3D": false,
+            "grade": "8.3",
+            "director": "导演：彭顺",
+            "starring": "主演：陈学冬 张孝全 桂纶镁"
+        },
+        {
+            "id": 13,
+            "img": "https://gw.alicdn.com/i2/TB1WRaufrYI8KJjy0FaXXbAiVXa_.jpg_160x160Q75.jpg",
+            "video": "http://vodcdn.video.taobao.com/oss/taobao-ugc/9c327c7ad15e4b70958660091c7606e7/1511777828/video.mp4",
+            "title": "鲨海",
+            "is3D": false,
+            "grade": "7.3",
+            "director": "导演：约翰内斯·罗伯茨",
+            "starring": "主演：曼迪·摩尔 克莱尔·霍尔特"
+        },
+        {
+            "id": 14,
+            "img": "https://gw.alicdn.com/i2/TB13OSKXMnD8KJjy1XdXXaZsVXa_.jpg_160x160Q75.jpg",
+            "video": "http://vodcdn.video.taobao.com/oss/taobao-ugc/34259914fb254d199a20370614c95bcc/1511918877/video.mp4",
+            "title": "解忧杂货店",
+            "is3D": false,
+            "grade": "7.3",
+            "director": "导演：韩杰",
+            "starring": "主演：王俊凯 迪丽热巴 董子健"
+        }
+
+    ]
+
+};
---
+++ server/server.js
@@ -0,0 +1,41 @@
+let express = require('express');
+let bodyParse = require('body-parser');
+let app = express();
+app.listen(8090);
+app.use(function(req,res,next){
+
+    //只允许3000端口访问
+    res.header('Access-Control-Allow-Origin','http://localhost:3000');
+    //服务允许客户端发的方法
+    res.header('Access-Control-Allow-Methods','GET,POST,DELETE,PUT');
+    //服务器允许的请求头
+    res.header('Access-Control-Allow-Headers','Content-Type,Accept');
+    //允许客户端把cookie发过来
+    res.header('Access-Control-Allow-Credentials','true');
+    //如果请求的方法是OPTIONS,那么意味着客户端只要响应头，直接结束响应即可
+    if(req.method === 'OPTIONS'){
+        res.end();
+    }else{
+        next();
+    }
+});
+let sliders = require('./mock/sliders');
+app.get('/api/sliders', function (req, res) {
+    res.json(sliders);
+});
+let movieList = require('./mock/movieList');
+app.get('/api/movieList', function(req, res){
+    //offset从哪请求，limit请求几条
+    let {offset = 0, limit = 5} = req.query;
+
+    offset = isNaN(offset) ? 0 : parseInt(offset);
+    limit = isNaN(limit) ? 0 : parseInt(limit);
+    let newMovies = JSON.parse(JSON.stringify(movieList));
+    console.log(newMovies === movieList);
+    //如果下一页的起始索引已经大于等于总条数了，则认为已经分页完毕，后面已经没有数据了
+    newMovies.hasMore = limit+offset < newMovies.list.length;
+    //提取指定页的数据
+    newMovies.list = newMovies.list.slice(offset, offset + limit);
+
+    res.json(newMovies);
+});
